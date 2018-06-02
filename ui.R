a6ad7f613444f827e7b92831ad9966e9539d314a
Vinayak.S.Karande <38550519+vinayakkarande@users.noreply.github.com>
2018-06-02 19:39:40 +0530

    Add files via upload

--- ui.R
+++ ui.R
@@ -12,7 +12,28 @@
 # 3.Vinayak Karande - 11910039
 # 
 # ---
-  
+#setup Required Libraries
+if (!require(shiny)){install.packages("shiny")}
+if (!require(udpipe)){install.packages("udpipe")}
+if (!require(textrank)){install.packages("textrank")}
+if (!require(lattice)){install.packages("lattice")}
+if (!require(igraph)){install.packages("igraph")}
+if (!require(ggraph)){install.packages("ggraph")}
+if (!require(gplot2)){install.packages("gplot2")}
+if (!require(wordcloud)){install.packages("wordcloud")}
+if (!require(stringr)){install.packages("stringr")}
+
+
+library(shiny)
+library(udpipe)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggraph)
+library(ggplot2)
+library(wordcloud)
+library(stringr)
+
 
 shinyUI(
   fluidPage(
